28fabbadc464e1ea9a396cdced1c58dc522c4469
Shea Spofford <spofd87@icloud.com>
2022-12-12 20:52:45 -0700

    ...

--- functions/todos.js
+++ functions/todos.js
@@ -2,9 +2,9 @@
 import path from 'path'
 import  fs  from 'fs/promises'
 
-export async function handler(event, context){
-     const filePath = path.resolve('./db/todos.json')
-     const data =    await fs.readFile(filePath, "utf-8")
+export async function handler(event, context){ 
+    const filePath = path.resolve('./db/todos.json')
+    const data = await fs.readFile(filePath, 'utf-8')
     return {
         statusCode: 200,
         body: data
--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,19 @@
+import {categoryTemplates} from './templates/categories'
 
-async function getToDos(){
-    const res = await fetch('/.netlify/functions/todos')
-    console.log(res)          
-    const data = await res.json()
-    console.log( data.todos)
+//  init app, load data, loop data, create the todos and them to the DOM.
+
+async function appInit(){
+   const res = await fetch('.netlify/functions/todos')
+   const todos = await res.json()
+   const containerElement = document.createElement('div')
+   let markup = ``
+   todos.forEach(todo =>{
+         const template = categoryTemplates[todo.category](todo)
+         markup += template
+         
+   })
+   containerElement.innerHTML = markup
+   document.querySelector('main').append(containerElement)
 }
 
-getToDos()
+appInit()
